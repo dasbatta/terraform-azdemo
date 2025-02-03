0556a7664ce8e3d821cad87fc26ca3057ef90dbc
Kabeer Das Batta <dbatta@Kabeers-MacBook-Pro.local>
2025-02-02 19:47:57 -0800

    changing Dynamic to Static

--- networking.tf
+++ networking.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "vm" {
   name                = "${var.prefix}-vm-pip"
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_virtual_network" "demo" {
